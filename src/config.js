01cd4fdcb071059316f81e7b92ba2f711e62c4fd
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-04-25 10:15:21 -0600

    🐛 fix db_port name env

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ export const PORT = process.env.PORT || 5001
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '1234'
-export const DB_PORT = process.env.PORT || 3306
+export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_NAME = process.env.DB_NAME || 'testing'
 export const IMGBB_API_KEY = process.env.IMGBB_API_KEY || EMPTY_STRING
